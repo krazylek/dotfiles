8486adadde15c16c8df8e1075f28eeda4bc18760
Benoit <krazylek@gmail.com>
2013-11-17 17:45:06 +0100

    added scripts for vim

--- init.sh
+++ init.sh
@@ -1,1 +1,12 @@
-git clone git://github.com/kongo2002/fsharp-vim.git bundle/fsharp      vim/bundle/fsharp-vim
+# fsharp hilighting
+git clone git://github.com/kongo2002/fsharp-vim.git vim/bundle/fsharp-vim
+# git integration
+git clone git://github.com/tpope/vim-fugitive.git vim/bundle/vim-fugitive
+# repl
+git clone git://github.com/jpalardy/vim-slime.git vim/bundle/vim-slime
+# omnisharp: .Net completion
+# git clone https://github.com/nosami/Omnisharp.git vim/bundle/omnisharp
+# surround with anythin: need to read doc
+# git clone https://github.com/tpope/vim-surround vim/bundle/vim-surround
+# find tool: need to read doc
+# git clone https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim
