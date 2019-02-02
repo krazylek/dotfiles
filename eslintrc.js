68a0721f49cd6bfeeb4c964083eff276bc271d15
benoit <krazylek@gmail.com>
2019-02-02 14:26:03 +1100

    eslint es version

--- eslintrc.js
+++ eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
-    'sourceType': 'module'
+    'sourceType': 'module',
+    "ecmaVersion": 2018
   },
   'rules': {
     'indent': [
