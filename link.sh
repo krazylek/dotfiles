3ff28120756e732c2316ff6cbd50b90d572fa37b
benoit <krazylek@gmail.com>
2019-02-25 18:17:23 +1100

    eslint

--- link.sh
+++ link.sh
@@ -1,7 +1,4 @@
-# Home conf
-ln -s $(pwd)/vim $HOME/.vim
-ln -s $(pwd)/vimrc $HOME/.vimrc
 # bashrc just for informations...
 # ln -s $(pwd)/bashrc $HOME/.bashrc
 # eslint conf
-ln -s $(pwd)/eslintrc.js $HOME/.eslintrc.js
+ln -s $(pwd)/eslintrc.json $HOME/.eslintrc
